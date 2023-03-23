f0eaad324041dbd9b135e2c9d7e3e815fc5cc52e
Moritz-Friedemann <mofriedemann@web.de>
2023-03-24 00:35:35 +0100

    friends list gets updated when tapping on the tab again

--- src/screens/FriendsScreen.tsx
+++ src/screens/FriendsScreen.tsx
@@ -1,7 +1,8 @@
+import { useIsFocused } from '@react-navigation/native'
+import Colors from 'constants/colors'
 import FriendCard from 'components/FriendCard'
-import { ListResult } from 'pocketbase'
 import { useEffect, useState } from 'react'
-import { FlatList, ListRenderItemInfo, StyleSheet, View } from 'react-native'
+import { FlatList, ListRenderItemInfo, StyleSheet, View, Text } from 'react-native'
 import { pb } from 'src/pocketbaseService'
 import { useAuthenticatedUser } from 'src/store/AuthenticatedUserContext'
 import { FriendsWithRecord, UserRecord } from 'types'
@@ -16,8 +17,14 @@ export default function FriendsScreen() {
     const [friends, setFriends] = useState<UserRecord[]>([])
     const { currentUser } = useAuthenticatedUser()
 
+    const isFocused: boolean = useIsFocused()
+
     useEffect(() => {
-        if (currentUser === null) return
+        if (!isFocused) return
+        if (currentUser === null) {
+            setFriends([])
+            return
+        }
         pb.collection("friends_with").getFullList<FriendsWithRecord>({
             filter: `user1.id= "${currentUser.id}" || user2.id = "${currentUser.id}"`,
         }).then((records: FriendsWithRecord[]) => {
@@ -28,7 +35,7 @@ export default function FriendsScreen() {
                 filter: ids.map((id: string) => `id="${id}"`).join("||")
             }).then(setFriends)
         }).catch((error) => console.error("An error occured while fetching the friends data", error))
-    }, [])
+    }, [isFocused])
 
     function renderFriend({ item }: ListRenderItemInfo<UserRecord>) {
         return <FriendCard user={item} />
@@ -36,6 +43,7 @@ export default function FriendsScreen() {
 
     return (
         <View style={styles.friendsContainer}>
+            {friends.length === 0 && <Text style={styles.noFriendsText}>You haven't added any friends yet</Text>}
             <FlatList
                 numColumns={2}
                 data={friends}
@@ -53,5 +61,11 @@ const styles = StyleSheet.create({
     friendsContainer: {
         flex: 1,
         backgroundColor: "black",
+        justifyContent: "center",
+    },
+    noFriendsText: {
+        color: Colors.accent,
+        flex: 1,
+        textAlign: "center",
     }
 })
