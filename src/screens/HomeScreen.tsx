33b78da3047b552c71107943ddc070e680fcd7b2
Moritz-Friedemann <mofriedemann@web.de>
2023-03-23 21:52:49 +0100

    redesign home page and warning for wrong login

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,10 @@
 import LabelledTextInput from "components/LabelledTextInput";
-import { RecordAuthResponse } from "pocketbase";
+import { ClientResponseError, RecordAuthResponse } from "pocketbase";
 import { pb } from "src/pocketbaseService";
 import { useState } from "react";
-import { View, Button, Text } from "react-native";
-import { styles } from "src/styles";
+import { View, Button, Text, StyleSheet, Alert } from "react-native";
 import { useAuthenticatedUser } from "src/store/AuthenticatedUserContext";
+import ProfilePicture from "components/ProfilePicture";
 
 export default function HomeScreen() {
     const { currentUser } = useAuthenticatedUser()
@@ -22,25 +22,50 @@ export default function HomeScreen() {
     }
 
     async function login(): Promise<void> {
-        const response: RecordAuthResponse = await pb.collection("users").authWithPassword(username, password)
+        pb.collection("users").authWithPassword(username, password)
+            .then((response: RecordAuthResponse) => { })
+            .catch((error: ClientResponseError) => Alert.alert("Wrong username or password. Please try again."))
     }
 
     async function logout(): Promise<void> {
         await pb.authStore.clear()
     }
 
-    return (
-        <View style={styles.container}>
-            <Text>{currentUser === null ? "Not signed in!" : "Signed in as " + currentUser.id}</Text>
-            <View style={styles.loginBox}>
-                <LabelledTextInput label="Username:" placeholder="username" onChangeText={(text: string) => setUsername(text)} style={styles.textInput} />
-                <LabelledTextInput label="Password:" placeholder='password' onChangeText={(text: string) => setPassword(text)} style={styles.textInput} secureTextEntry />
-            </View>
-            <View>
+    if (currentUser === null) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.signedInText}>Not signed in.</Text>
+                <View style={styles.loginBox}>
+                    <LabelledTextInput label="Username:" placeholder="username" onChangeText={(text: string) => setUsername(text)} />
+                    <LabelledTextInput label="Password:" placeholder='password' onChangeText={(text: string) => setPassword(text)} secureTextEntry />
+                </View>
                 <Button title="Sign Up" onPress={signup}></Button>
                 <Button title="Log in" onPress={login}></Button>
-                <Button title="Log out" onPress={logout}></Button>
             </View>
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            <ProfilePicture user={currentUser} style={{ width: 150, height: 150 }} />
+            <Text style={styles.signedInText}>Signed in as {currentUser.name}</Text>
+            <Button title="Log out" onPress={logout}></Button>
         </View>
     )
 }
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 100,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    loginBox: {
+        margin: 5,
+    },
+    signedInText: {
+        marginBottom: 50,
+    }
+});
