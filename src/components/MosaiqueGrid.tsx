416a4e6a3bceda7b89128e1807a0ce6db5aa5ad0
Moritz-Friedemann <mofriedemann@web.de>
2023-08-23 06:53:06 -0400

    add floating shaking picture

--- src/components/MosaiqueGrid.tsx
+++ src/components/MosaiqueGrid.tsx
@@ -1,11 +1,15 @@
+import { RouteProp, useRoute } from "@react-navigation/native";
 import { useCallback } from "react";
 import { StyleSheet, View } from "react-native";
 import { useMosaiqueData } from "src/store/MosaiqueDataProvider";
+import FloatingPicture from "./FloatingPicture";
+import { AppParamList } from "./LoggedInApp";
 import MosaiqueTile from "./MosaiqueTile";
 
 export default function MosaiqueGrid() {
     const { numRings } = useMosaiqueData()
     const gridSize: number = 2 * numRings + 1
+    const { params } = useRoute<RouteProp<AppParamList, "Mosaique">>()
 
     const getIndices = useCallback(
         function (i: number, j: number) {
@@ -15,6 +19,7 @@ export default function MosaiqueGrid() {
 
     return (
         <View style={styles.container}>
+            {params && params.imageUri && <FloatingPicture imageUri={params.imageUri} />}
             {
                 Array.from({ length: gridSize }).map((_: any, rowIndex: number) => (
                     <View style={styles.row} key={rowIndex}>
