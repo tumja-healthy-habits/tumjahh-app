c83185276ff025534c0b49bbbcfec78c9a5fab36
Moritz-Friedemann <mofriedemann@web.de>
2023-11-14 12:01:15 -0500

    reuse forgot password label

---
+++ src/components/ForgotPasswordLabel.tsx
@@ -0,0 +1,33 @@
+import Colors from "constants/colors";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+type ForgotPasswordLabelProps = {
+    textLabel: string;
+    buttonLabel: string;
+    onPress: () => void;
+};
+
+export default function ForgotPasswordLabel({ textLabel, buttonLabel, onPress }: ForgotPasswordLabelProps) {
+    return <View style={styles.container}>
+        <Text style={styles.text}>{textLabel}</Text>
+        <Pressable onPress={onPress} style={({ pressed }) => pressed && { opacity: 0.3 }}>
+            <Text style={styles.buttonText}>{buttonLabel}</Text>
+        </Pressable>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginBottom: 30,
+    },
+    text: {
+        color: Colors.accent,
+        marginRight: 5,
+    },
+    buttonText: {
+        color: '#FFF4EC',
+        fontWeight: '700'
+    }
+})
