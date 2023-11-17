bafc33a8e55f3a5fd0050cbacee8c83460ef53b0
Moritz-Friedemann <mofriedemann@web.de>
2023-11-17 06:37:35 -0500

    update search results whenever the input text changes

--- src/components/profile/FriendSearch.tsx
+++ src/components/profile/FriendSearch.tsx
@@ -1,9 +1,9 @@
 import { Ionicons } from "@expo/vector-icons";
 import Colors from "constants/colors";
 import { Contact, Fields as ContactFields, PermissionStatus, PhoneNumber, getContactsAsync, requestPermissionsAsync } from "expo-contacts";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, ListRenderItemInfo, Text, View } from "react-native";
-import { Divider, TextInput } from "react-native-paper";
+import { Divider, TextInput, Tooltip } from "react-native-paper";
 import { pb } from "src/pocketbaseService";
 import { globalStyles } from "src/styles";
 import { UserRecord } from "types";
@@ -20,60 +20,65 @@ export default function FriendSearch({ showQRCode }: FriendSearchProps) {
 
     console.log(searchResults, "\nshowing results: ", showResults)
 
-    async function submitSearch() {
-        if (searchInput.length > 0) {
-            // USERS
-            setShowResults(true)
-            const foundByUsername: UserRecord[] = (await pb.collection("users").getFullList<UserRecord>({
-                filter: `username ~ "${searchInput}"`
-            }))
-            console.log("found by username", foundByUsername.map((user: UserRecord) => user.username))
+    useEffect(() => {
+        submitSearch()
+    }, [searchInput])
 
-            // contacts permission
-            const { status } = await requestPermissionsAsync()
-            if (status === PermissionStatus.GRANTED) {
-                // getting contacts
-                const { data } = await getContactsAsync({
-                    name: searchInput,
-                    fields: [ContactFields.PhoneNumbers]
-                })
-                // extracting numbers from contacts --> creating filter
-                const phoneNumberQuery: string = data.map((contact: Contact) => {
-                    // extracting phone numbers
-                    if (contact.phoneNumbers === undefined) return null
-                    return contact.phoneNumbers.map(
-                        (numberEntry: PhoneNumber) => {
-                            // unpacking and formatting number
-                            if (numberEntry.number === undefined) return null
-                            let number: string = numberEntry.number.replaceAll(/[^+0-9]/g, "")
-                            // checking for country code (+49 fallback)
-                            if (number.startsWith("00")) {
-                                number = "+" + number.substring(2)
-                            } else if (number.startsWith("0")) {
-                                number = "+49" + number.substring(1)
-                            } else if (!number.startsWith("+")) {
-                                number = "+49" + number
-                            }
-                            return `phoneNumber = "${number}"`
+    async function submitSearch() {
+        if (searchInput.length === 0) {
+            setShowResults(false)
+            setSearchResults([])
+            return
+        }
+        // USERS
+        setShowResults(true)
+        const foundByUsername: UserRecord[] = (await pb.collection("users").getFullList<UserRecord>({
+            filter: `username ~ "${searchInput}"`
+        }))
+
+        // contacts permission
+        const { status } = await requestPermissionsAsync()
+        if (status === PermissionStatus.GRANTED) {
+            // getting contacts
+            const { data } = await getContactsAsync({
+                name: searchInput,
+                fields: [ContactFields.PhoneNumbers]
+            })
+            // extracting numbers from contacts --> creating filter
+            const phoneNumberQuery: string = data.map((contact: Contact) => {
+                // extracting phone numbers
+                if (contact.phoneNumbers === undefined) return null
+                return contact.phoneNumbers.map(
+                    (numberEntry: PhoneNumber) => {
+                        // unpacking and formatting number
+                        if (numberEntry.number === undefined) return null
+                        let number: string = numberEntry.number.replaceAll(/[^+0-9]/g, "")
+                        // checking for country code (+49 fallback)
+                        if (number.startsWith("00")) {
+                            number = "+" + number.substring(2)
+                        } else if (number.startsWith("0")) {
+                            number = "+49" + number.substring(1)
+                        } else if (!number.startsWith("+")) {
+                            number = "+49" + number
                         }
-                    )
-                }
-                ).flat()
-                    .filter((number: string | null) => number != null)
-                    .join(" || ")
-                console.log("query: ", phoneNumberQuery, phoneNumberQuery.length)
-                if (phoneNumberQuery.length === 0) {
-                    setSearchResults(foundByUsername)
-                    return
-                }
-                // Matching phone numbers
-                pb.collection("users").getFullList<UserRecord>({
-                    filter: `username !~ "${searchInput}" && (${phoneNumberQuery})`
-                }).then((records: UserRecord[]) => {
-                    // console.log("found by phone numbers", records)
-                    setSearchResults(foundByUsername.concat(records))
-                }).catch(console.error)
+                        return `phoneNumber = "${number}"`
+                    }
+                )
             }
+            ).flat()
+                .filter((number: string | null) => number != null)
+                .join(" || ")
+            if (phoneNumberQuery.length === 0) {
+                setSearchResults(foundByUsername)
+                return
+            }
+            // Matching phone numbers
+            pb.collection("users").getFullList<UserRecord>({
+                filter: `username !~ "${searchInput}" && (${phoneNumberQuery})`
+            }).then((records: UserRecord[]) => {
+                // console.log("found by phone numbers", records)
+                setSearchResults(foundByUsername.concat(records))
+            }).catch(console.error)
         }
     }
 
@@ -86,11 +91,10 @@ export default function FriendSearch({ showQRCode }: FriendSearchProps) {
         <TextInput value={searchInput}
             placeholder="Search for your friend's username"
             onChangeText={setSearchInput}
-            onSubmitEditing={submitSearch}
             autoCapitalize="none"
             autoCorrect={false}
             left={<TextInput.Icon icon={() => <Ionicons name="search-outline" size={24} color="black" />} />}
-            right={<TextInput.Icon icon={() => <Ionicons name="qr-code-outline" size={24} color="black" onPress={showQRCode} />} />}
+            right={<TextInput.Icon icon={() => <Tooltip title="Show QR code"><Ionicons name="qr-code-outline" size={24} color="black" onPress={showQRCode} /></Tooltip>} />}
             style={{ backgroundColor: Colors.pastelGreen, marginHorizontal: 10 }}
 
         />
