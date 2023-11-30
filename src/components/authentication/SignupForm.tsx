e8a72cab692e886d06a56bb819ab253ca69bc93e
Moritz-Friedemann <mofriedemann@web.de>
2023-11-29 19:37:21 -0500

    small fix

--- src/components/authentication/SignupForm.tsx
+++ src/components/authentication/SignupForm.tsx
@@ -1,21 +1,20 @@
+import { Ionicons } from '@expo/vector-icons';
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import Colors from "constants/colors";
-import React, { useState, useRef } from 'react';
-import { Alert, Image, StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler'
-import DropDownPicker from 'react-native-dropdown-picker';
+import React, { useRef, useState } from 'react';
+import { Image, KeyboardAvoidingView, Platform, StyleSheet, Text, View } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import { ScrollView } from 'react-native-gesture-handler';
+import { RadioButton } from 'react-native-paper';
 import PhoneInput from "react-native-phone-number-input";
-import { Ionicons } from '@expo/vector-icons';
-import {Dropdown} from 'react-native-element-dropdown';
-import {RadioButton} from 'react-native-paper';
 
 
 import { signup } from "src/authentification";
 
-import LoginButton from './LoginButton';
-import { CalendarInput, FormTextInput, ProfilePictureInput} from './InputField';
-import { LoginParamList } from "./LoginNavigator";
 import { FixedDimensionImage } from "types";
+import { CalendarInput, FormTextInput, ProfilePictureInput } from './InputField';
+import LoginButton from './LoginButton';
+import { LoginParamList } from "./LoginNavigator";
 
 
 import { globalStyles } from 'src/styles';
@@ -30,13 +29,13 @@ export default function SignupForm() {
 	const [passwordConfirm, setPasswordConfirm] = useState<string>("")
 	const [gender, setGender] = useState<string>("");
 	const genderOptions = [
-		{label: 'Male', value: 'male'},
-		{label: 'Female', value: 'female'},
-		{label: 'Diverse', value: 'diverse'},
-		{label: 'Not specified', value: 'not specified'}
+		{ label: 'Male', value: 'male' },
+		{ label: 'Female', value: 'female' },
+		{ label: 'Diverse', value: 'diverse' },
+		{ label: 'Not specified', value: 'not specified' }
 	];
 	const [phoneNumber, setPhoneNumber] = useState("");
-  	const [formattedPhoneNumber, setFormattedPhoneNumber] = useState("");
+	const [formattedPhoneNumber, setFormattedPhoneNumber] = useState("");
 	const phoneInput = useRef<PhoneInput>(null);
 	const [birthdate, setBirthdate] = useState(new Date())
 	const [openDatepicker, setOpenDatepicker] = useState(false)
@@ -53,22 +52,22 @@ export default function SignupForm() {
 
 
 	function handleSignup(): void {
-		signup(username, 
-			name ? name : username, 
-			email, 
-			password, 
-			passwordConfirm, 
-			formattedPhoneNumber, 
-			gender, birthdate, 
-			isStudent === 'yes' ? true : false, 
+		signup(username,
+			name ? name : username,
+			email,
+			password,
+			passwordConfirm,
+			formattedPhoneNumber,
+			gender, birthdate,
+			isStudent === 'yes' ? true : false,
 			profilePicture !== undefined ? {
 				uri: profilePicture!.uri,
 				name: profilePicture!.uri,
 				type: "image/jpg"
-        	} : {})
+			} : {})
 	}
 
-	const onChangeDate = (event:any, selectedDate:any) => {
+	const onChangeDate = (event: any, selectedDate: any) => {
 		const currentDate = selectedDate;
 		setBirthdate(currentDate);
 	};
@@ -77,135 +76,135 @@ export default function SignupForm() {
 	return (
 		//<ScrollView contentContainerStyle={[globalStyles.container, styles.outerContainer]}>
 		<KeyboardAvoidingView
-                    behavior={Platform.OS === "ios" ? "padding" : null}
-                    style={[globalStyles.container, styles.outerContainer]}>
+			behavior={Platform.OS === "ios" ? "padding" : undefined}
+			style={[globalStyles.container, styles.outerContainer]}>
 			<View style={styles.innerContainer}>
 				<View style={styles.headerContainer}>
 					<Text style={styles.formTitle}>Register</Text>
 					<Image source={require("assets/images/behealthy-icon.png")} style={{ width: 170, height: 170, alignSelf: 'flex-end' }} />
 				</View>
-				
+
 				<ScrollView>
-				<FormTextInput
-					label={'Username'}
-					iconName="person-outline"
-					onChangeText={setUsername}
-					mandatory={true}
-				/>
-
-				<FormTextInput
-					label={'Name'}
-					iconName="person-outline"
-					onChangeText={setName}
-					mandatory={false}
-				/>
-
-				<FormTextInput
-					label={'Email'}
-					iconName="at-outline"
-					keyboardType="email-address"
-					onChangeText={setEmail}
-					mandatory={true}
-				/>
-
-				<View style={styles.inputFieldContainer}>
-					<Ionicons name={'ios-call-outline'} size={20} color="#666" style={{ marginRight: 5,}} />
-					{/* <StarIcon iconName={'ios-call-outline'} /> */}
-					<PhoneInput
-						ref={phoneInput}
-						defaultValue={phoneNumber}
-						defaultCode="DE"
-						layout="second"
-						onChangeText={(text) => {
-						setPhoneNumber(text);
-						}}
-						onChangeFormattedText={(text) => {
-						setFormattedPhoneNumber(text);
-						}}
-						containerStyle={styles.phoneNumberContainer}
-						textContainerStyle={styles.phoneNumberContainer}
-						textInputStyle={styles.inputText}
+					<FormTextInput
+						label={'Username'}
+						iconName="person-outline"
+						onChangeText={setUsername}
+						mandatory={true}
 					/>
-				</View>
 
-				<FormTextInput
-					label={'Password'}
-					iconName="ios-lock-closed-outline"
-					inputType="password"
-					onChangeText={setPassword}
-					mandatory={true}
-				/>
-
-				<FormTextInput
-					label={'Confirm Password'}
-					iconName="ios-lock-closed-outline"
-					inputType="password"
-					onChangeText={setPasswordConfirm}
-					mandatory={true}
-				/>
-
-				<View style={{flexDirection:'row', justifyContent: 'space-between',}}>
-					<CalendarInput 
-						label="Birthdate"
-						iconName="calendar-outline"
-						onChangeDate={onChangeDate}
-						mode={"date"}
-						onPress={() => setOpenDatepicker(true)}
-						value={birthdate}
-						openPicker={openDatepicker}
+					<FormTextInput
+						label={'Name'}
+						iconName="person-outline"
+						onChangeText={setName}
+						mandatory={false}
 					/>
-					<Dropdown
-						style={styles.dropdown}
-						placeholderStyle={styles.inputText}
-						//selectedTextStyle={styles.selectedTextStyle}
-						//inputSearchStyle={styles.inputSearchStyle}
-						//iconStyle={styles.iconStyle}
-						data={genderOptions}
-						//search
-						maxHeight={300}
-						labelField="label"
-						valueField="value"
-						placeholder={'Gender *'}
-						value={gender}
-						//onFocus={() => setIsFocus(true)}
-						//onBlur={() => setIsFocus(false)}
-						onChange={(item) => {
-							setGender(item.value);
-							//setIsFocus(false);
-						}}
-					/>
-				</View>
 
-				<View style={{flexDirection:'row', justifyContent: 'space-between', marginBottom:25}}>
-					<View style={{width:'50%'}}>
-						<Text style={{fontSize:16}}>Are you a student? *</Text>
-						<View style={{width:'50%'}}>
-							<RadioButton.Group
-								onValueChange={(value) => setIsStudent(value)}
-								value={isStudent}
-							>
-								<RadioButton.Item label="Yes" value="yes" 
-									style={styles.radioButton} 
-									labelStyle={styles.radioButtonLabel}
-									color="#FFF4EC"/>
-								<RadioButton.Item label="No" value="no" 
-									style={styles.radioButton}
-									labelStyle={styles.radioButtonLabel}
-									color="#FFF4EC"/>
-							</RadioButton.Group>
+					<FormTextInput
+						label={'Email'}
+						iconName="at-outline"
+						keyboardType="email-address"
+						onChangeText={setEmail}
+						mandatory={true}
+					/>
+
+					<View style={styles.inputFieldContainer}>
+						<Ionicons name={'ios-call-outline'} size={20} color="#666" style={{ marginRight: 5, }} />
+						{/* <StarIcon iconName={'ios-call-outline'} /> */}
+						<PhoneInput
+							ref={phoneInput}
+							defaultValue={phoneNumber}
+							defaultCode="DE"
+							layout="second"
+							onChangeText={(text) => {
+								setPhoneNumber(text);
+							}}
+							onChangeFormattedText={(text) => {
+								setFormattedPhoneNumber(text);
+							}}
+							containerStyle={styles.phoneNumberContainer}
+							textContainerStyle={styles.phoneNumberContainer}
+							textInputStyle={styles.inputText}
+						/>
+					</View>
+
+					<FormTextInput
+						label={'Password'}
+						iconName="ios-lock-closed-outline"
+						inputType="password"
+						onChangeText={setPassword}
+						mandatory={true}
+					/>
+
+					<FormTextInput
+						label={'Confirm Password'}
+						iconName="ios-lock-closed-outline"
+						inputType="password"
+						onChangeText={setPasswordConfirm}
+						mandatory={true}
+					/>
+
+					<View style={{ flexDirection: 'row', justifyContent: 'space-between', }}>
+						<CalendarInput
+							label="Birthdate"
+							iconName="calendar-outline"
+							onChangeDate={onChangeDate}
+							mode={"date"}
+							onPress={() => setOpenDatepicker(true)}
+							value={birthdate}
+							openPicker={openDatepicker}
+						/>
+						<Dropdown
+							style={styles.dropdown}
+							placeholderStyle={styles.inputText}
+							//selectedTextStyle={styles.selectedTextStyle}
+							//inputSearchStyle={styles.inputSearchStyle}
+							//iconStyle={styles.iconStyle}
+							data={genderOptions}
+							//search
+							maxHeight={300}
+							labelField="label"
+							valueField="value"
+							placeholder={'Gender *'}
+							value={gender}
+							//onFocus={() => setIsFocus(true)}
+							//onBlur={() => setIsFocus(false)}
+							onChange={(item) => {
+								setGender(item.value);
+								//setIsFocus(false);
+							}}
+						/>
+					</View>
+
+					<View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 25 }}>
+						<View style={{ width: '50%' }}>
+							<Text style={{ fontSize: 16 }}>Are you a student? *</Text>
+							<View style={{ width: '50%' }}>
+								<RadioButton.Group
+									onValueChange={(value) => setIsStudent(value)}
+									value={isStudent}
+								>
+									<RadioButton.Item label="Yes" value="yes"
+										style={styles.radioButton}
+										labelStyle={styles.radioButtonLabel}
+										color="#FFF4EC" />
+									<RadioButton.Item label="No" value="no"
+										style={styles.radioButton}
+										labelStyle={styles.radioButtonLabel}
+										color="#FFF4EC" />
+								</RadioButton.Group>
+							</View>
+						</View>
+						<View style={{ width: "50%" }}>
+							<ProfilePictureInput onTakePhoto={setProfilePicture} profilePicture={profilePicture} />
 						</View>
 					</View>
-					<View style={{width:"50%"}}>
-						<ProfilePictureInput onTakePhoto={setProfilePicture} profilePicture={profilePicture}/>
-					</View>
-				</View>
-			
-				<LoginButton label={'Register'} onPress={handleSignup} />
-				<ForgotPasswordLabel
-					textLabel="Already registered?"
-					buttonLabel="Login"
-					onPress={goBack}
-				/>
+
+					<LoginButton label={'Register'} onPress={handleSignup} />
+					<ForgotPasswordLabel
+						textLabel="Already registered?"
+						buttonLabel="Login"
+						onPress={goBack}
+					/>
 				</ScrollView>
 
 			</View>
@@ -221,13 +220,13 @@ const styles = StyleSheet.create({
 		//paddingTop:'40%',
 		flexGrow: 1,
 	},
-	innerContainer:{
-        flex: 1,
-        justifyContent: "center",
-        width: "90%",
-		marginTop:"15%", 
-		paddingBottom:"15%"
-    },
+	innerContainer: {
+		flex: 1,
+		justifyContent: "center",
+		width: "90%",
+		marginTop: "15%",
+		paddingBottom: "15%"
+	},
 	headerContainer: {
 		flexDirection: 'row',
 		justifyContent: 'space-between',
@@ -259,27 +258,27 @@ const styles = StyleSheet.create({
 	phoneNumberContainer: {
 		backgroundColor: 'transparent',
 		alignSelf: 'flex-start',
-		paddingTop:0,
-		paddingLeft:0
+		paddingTop: 0,
+		paddingLeft: 0
 	},
 	dropdown: {
-		width:'45%',
-		borderColor:'#FFF4EC',
-		borderRadius:8,
+		width: '45%',
+		borderColor: '#FFF4EC',
+		borderRadius: 8,
 		borderWidth: 1,
 		paddingHorizontal: 8,
-		marginBottom:25
+		marginBottom: 25
 	},
 	radioButton: {
 		borderWidth: 1,
-		borderColor:'#FFF4EC',
-		borderRadius:8,
-		margin:5,
-		paddingVertical:0,
-		paddingLeft:7,
-		paddingRight:0,
+		borderColor: '#FFF4EC',
+		borderRadius: 8,
+		margin: 5,
+		paddingVertical: 0,
+		paddingLeft: 7,
+		paddingRight: 0,
 	},
 	radioButtonLabel: {
-		paddingHorizontal:0
+		paddingHorizontal: 0
 	},
 })
